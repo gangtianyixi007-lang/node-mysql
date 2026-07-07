678d45d6f37b0f46134c834037336b128f956a8e
gg <gangtianyixi007@gmail.com>
2026-07-07 08:23:41 +0000

    18

--- routes/logout.js
+++ routes/logout.js
@@ -2,8 +2,20 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function (req, res, next) {
-  req.session = null;
-  res.redirect('/');
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+
+    req.session.userid = null;
+    req.session.destroy(function (sessionErr) {
+      if (sessionErr) {
+        return next(sessionErr);
+      }
+
+      res.redirect('/');
+    });
+  });
 });
 
 module.exports = router;
--- routes/signin.js
+++ routes/signin.js
@@ -1,54 +1,37 @@
 const express = require('express');
 const router = express.Router();
-const knex = require("../db/knex");
-const bcrypt = require("bcrypt");
+const passport = require("passport");
 
 router.get('/', function (req, res, next) {
-    const userId = req.session.userid;
-    const isAuth = Boolean(userId);
+  const userId = req.session.userid;
+  const isAuth = Boolean(userId);
   res.render("signin", {
     title: "Sign in",
     isAuth: isAuth,
+    errorMessage: req.flash("error"),
   });
 });
 
 router.post('/', function (req, res, next) {
-    const userId = req.session.userid;
-    const isAuth = Boolean(userId);
-  const username = req.body.username;
-  const password = req.body.password;
+  passport.authenticate('local', function (err, user, info) {
+    if (err) {
+      return next(err);
+    }
 
-  knex("users")
-    .where({
-      name: username,
-    })
-    .select("*")
-    .then(async function (results) {
-      if (results.length === 0) {
-        res.render("signin", {
-          title: "Sign in",
-          errorMessage: ["ユーザが見つかりません"],
-          isAuth: isAuth,
-        });
-      } else if (await bcrypt.compare(password, results[0].password)) {
-        req.session.userid = results[0].id;
-        res.redirect('/');
-      } else {
-        res.render("signin", {
-          title: "Sign in",
-          errorMessage: ["ユーザが見つかりません"],
-          isAuth: isAuth,
-        });
+    if (!user) {
+      req.flash("error", info && info.message ? info.message : "Invalid User");
+      return res.redirect('/signin');
+    }
+
+    req.logIn(user, function (loginErr) {
+      if (loginErr) {
+        return next(loginErr);
       }
-    })
-    .catch(function (err) {
-      console.error(err);
-      res.render("signin", {
-        title: "Sign in",
-        errorMessage: [err.sqlMessage],
-        isAuth: isAuth,
-      });
+
+      req.session.userid = user.id;
+      return res.redirect('/');
     });
+  })(req, res, next);
 });
 
 module.exports = router;
